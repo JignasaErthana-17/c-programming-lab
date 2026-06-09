61049185eac5e6cc3a41abe99964a6ea901f9fe0
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-09 15:38:20 +0530

    palindrome_number  program

---
+++ Loops/palindrome_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, rev = 0, rem, temp;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(n != 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n /= 10;
+    }
+
+    if(temp == rev)
+        printf("Palindrome Number\n");
+    else
+        printf("Not Palindrome Number\n");
+
+    return 0;
+}
