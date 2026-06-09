9b2ec68751e658caf82ce500f2201fdb04281935
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-09 16:04:14 +0530

    Create structure_declaration.c

---
+++ Structure/structure_declaration.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct Student
+{
+    int rollNo;
+    char name[50];
+    float marks;
+};
+
+int main()
+{
+    struct Student s;
+
+    printf("Structure Declared Successfully");
+
+    return 0;
+}
