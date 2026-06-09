ce4e7c86c88fa8a84789ac5afe4832c807594045
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-09 15:45:50 +0530

    Create largest_element.c

---
+++ Array/largest_element.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5];
+    int max;
+
+    printf("Enter 5 Elements:\n");
+
+    for(int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+
+    for(int i = 1; i < 5; i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    printf("Largest Element = %d", max);
+
+    return 0;
+}
