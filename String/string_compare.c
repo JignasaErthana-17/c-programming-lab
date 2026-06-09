dda5a670550c8266d7783c996c6bdef8f1de43ad
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-09 15:51:29 +0530

    Create string_compare.c

---
+++ String/string_compare.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[100], str2[100];
+
+    printf("Enter first string: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    printf("Enter second string: ");
+    fgets(str2, sizeof(str2), stdin);
+
+    if(strcmp(str1, str2) == 0)
+        printf("Strings are Equal");
+    else
+        printf("Strings are Not Equal");
+
+    return 0;
+}
