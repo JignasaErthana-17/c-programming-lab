06810e4ac9896a351deb34327016929992747d57
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-09 15:59:24 +0530

    Create call_by_reference.c

---
+++ function/call_by_reference.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void swap(int *x, int *y);
+
+int main()
+{
+    int a = 10, b = 20;
+
+    printf("Before Swap:\n");
+    printf("a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After Swap:\n");
+    printf("a = %d, b = %d\n", a, b);
+
+    return 0;
+}
+
+void swap(int *x, int *y)
+{
+    int temp;
+
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
