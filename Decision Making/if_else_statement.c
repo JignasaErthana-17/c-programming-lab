312ba3a60062741b1783211bd9be05bc4c81401f
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-10 15:13:48 +0530

    Create if_else_statement.c

---
+++ Decision Making/if_else_statement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int number = 7;
+
+    if (number % 2 == 0) {
+        printf("Even Number\n");
+    } else {
+        printf("Odd Number\n");
+    }
+
+    return 0;
+}
