296fa14a3ae861dae093e505db4d67aa3f17e22c
Jignasa Erthana <jignasa.thakor@gmail.com>
2026-06-10 15:10:13 +0530

    Create pascal_triangle.c

---
+++ pattern/pascal_triangle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+    int coef;
+
+    for(int i = 0; i < rows; i++) {
+
+        for(int space = 1; space < rows - i; space++) {
+            printf(" ");
+        }
+
+        coef = 1;
+
+        for(int j = 0; j <= i; j++) {
+            printf("%d ", coef);
+            coef = coef * (i - j) / (j + 1);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
